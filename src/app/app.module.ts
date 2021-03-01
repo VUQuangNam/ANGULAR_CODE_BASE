e7b715b08b424a05ac01350cb87b87a235ebd32b
VQN <nam.vq@consultindochina.com>
2021-03-01 14:27:59 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { PartialsModule } from 'src/partials/partials.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateComponent } from './create/create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './list/list.component';
+import { PartialsModule } from 'src/base/partials.module';
 
 @NgModule({
     declarations: [
